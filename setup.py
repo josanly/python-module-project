8bbc5dc625ee6249c26d3bd1265f554cd9fb0488
Adrien Josso <josso.adrien@gmail.com>
2017-07-26 21:22:10 +0200

    small setup

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='sample',
+      version='1.0',
+      description='an example of python module',
+      url='http://github.com/josanly/python-module-project',
+      author='Adrien Josso',
+      author_email='josso.adrien@gmail.com',
+      license='GPL v3.0',
+      packages=['sample'],
+      zip_safe=False)
+
